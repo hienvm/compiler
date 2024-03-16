b7b270906f4c90dc74cd17166219954155e7466c
mhien2804-vnu <21020198@vnu.edu.vn>
2024-03-17 01:21:35 +0700

    initial structure

---
+++ lexical_analyzer/__main__.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+import os
+from argparse import ArgumentParser, FileType
+
+
+def main():
+    argparser = ArgumentParser(description="Phân tích từ vựng.")
+    argparser.add_argument('-l', '--lex', nargs='?', default="lex.dat",
+                           help="Tên file automaton data cho lexical analyzer (.dat, mặc định lex.dat)")
+    args = argparser.parse_args()
+    print(os.path.abspath(Path(__file__, '../..', 'automaton_data', args.lex)))
+
+if __name__ == "__main__":
+    main()
